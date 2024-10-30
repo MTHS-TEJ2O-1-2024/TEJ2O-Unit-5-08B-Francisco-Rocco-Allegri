a7a1833917b35d2514abd871e5c766398ff302f5
Francisco-Rocco-Allegri <francisco.rocco-allegri@mths.ca>
2024-10-30 17:53:21 -0400

    created test sensor + stepper code

--- main.ts
+++ main.ts
@@ -5,17 +5,24 @@
  * This program ...
 */
 
+let distanceToObject: number = 10
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 
 
 
-while (true) {
-    
+while (distanceToObject >= 10) {
+
+    distanceToObject = sonar.ping
+        (
+            DigitalPin.P1,
+            DigitalPin.P2,
+            PingUnit.Centimeters
+        )
+
     basic.showIcon(IconNames.Yes)
     robotbit.StpCarMove(10, 48)
     basic.pause(500)
     robotbit.StpCarMove(-10, 48)
 }
-
-
